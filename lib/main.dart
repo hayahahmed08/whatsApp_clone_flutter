dea1e76e577f17dda3e0374e6571e786e62d6be4
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-08-24 16:46:43 +0500

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:whatsapp/home_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
      primarySwatch: Colors.teal,
       ),
-
+      home: HomeScreen(),
     );
   }
 }
