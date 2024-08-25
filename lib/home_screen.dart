93e2e156da6af27b8cf6714080d01fad88521b71
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-08-25 17:18:32 +0500

    popup values

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,34 +11,54 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3, // Number of tabs
+      length: 4, // Number of tabs (adjusted to 4)
       child: Scaffold(
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             'WhatsApp',
             style: TextStyle(color: Colors.white),
           ),
           backgroundColor: Colors.teal,
-          bottom: TabBar(
+          bottom: const TabBar(
             labelColor: Colors.white,
             unselectedLabelColor: Colors.white,
             tabs: [
-              Icon(Icons.camera_alt,color: Colors.white,),
-              Tab(text: 'Chats', ),
+              Tab(icon: Icon(Icons.camera_alt, color: Colors.white)),
+              Tab(text: 'Chats'),
               Tab(text: 'Status'),
               Tab(text: 'Call'),
             ],
           ),
-          actions:[
-            Icon(Icons.search, color: Colors.white,),
-            SizedBox(width: 10,),
-            Icon(Icons.more_vert_outlined, color: Colors.white,),
-            SizedBox(width: 10,),
-          ]
+          actions: [
+            const Icon(Icons.search, color: Colors.white),
+            const SizedBox(width: 10),
+            PopupMenuButton(
+              icon: const Icon(Icons.more_vert_outlined, color: Colors.white),
+              itemBuilder: (context) => [
+                const PopupMenuItem(
+                  value: 0,
+                  child: Text("New group"),
+                ),
+                const PopupMenuItem(
+                  value: 1,
+                  child: Text("Settings"),
+                ),
+                const PopupMenuItem(
+                  value: 2,
+                  child: Text("Logout"),
+                ),
+
+              ],
+              onSelected: (item) => {
+                // Handle menu item clicks here
+              },
+            ),
+            const SizedBox(width: 10),
+          ],
         ),
-        body: TabBarView(
+        body: const TabBarView(
           children: [
-            Icon(Icons.camera_alt,),
+            Icon(Icons.camera_alt),
             Center(child: Text('Chats Tab')),
             Center(child: Text('Status Tab')),
             Center(child: Text('Call Tab')),
