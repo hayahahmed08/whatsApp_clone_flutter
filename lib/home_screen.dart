0e8df7d4ae4a3f0ce3399f27b35302055ae641da
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-08-25 17:12:41 +0500

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,10 +10,40 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('WhatsApp', style: TextStyle(color: Colors.white),)),
-        backgroundColor: Colors.teal,
+    return DefaultTabController(
+      length: 3, // Number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'WhatsApp',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.teal,
+          bottom: TabBar(
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white,
+            tabs: [
+              Icon(Icons.camera_alt,color: Colors.white,),
+              Tab(text: 'Chats', ),
+              Tab(text: 'Status'),
+              Tab(text: 'Call'),
+            ],
+          ),
+          actions:[
+            Icon(Icons.search, color: Colors.white,),
+            SizedBox(width: 10,),
+            Icon(Icons.more_vert_outlined, color: Colors.white,),
+            SizedBox(width: 10,),
+          ]
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.camera_alt,),
+            Center(child: Text('Chats Tab')),
+            Center(child: Text('Status Tab')),
+            Center(child: Text('Call Tab')),
+          ],
+        ),
       ),
     );
   }
